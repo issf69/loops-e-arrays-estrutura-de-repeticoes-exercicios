0dd2322cc90ba2b9935566c5563b65d1c031c4c5
issf69 <irenesilvafranca2016@gmail.com>
2022-06-18 17:52:02 -0300

    first commit

--- src/dio/me/exercicios/arrays/Ex3_NumerosAleatorios.java
+++ src/dio/me/exercicios/arrays/Ex3_NumerosAleatorios.java
@@ -1,10 +1,38 @@
 package dio.me.exercicios.arrays;
 
-/**
- *
- */
+import javax.swing.plaf.synth.SynthOptionPaneUI;
+import java.util.Random;
+
+/*
+Faça um Programa que leia 20 números inteiros aleatórios (entre 0 e 100) armazene-os num vetor.
+Ao final mostre os números e seus sucessores.
+*/
 public class Ex3_NumerosAleatorios {
-public static void main(String[]args){
-        System.out.println("Hello world!");
+        public static void main(String[] args) {
+                Random random = new Random();
+
+                int[] numerosAleatorios = new int[20];
+
+                for (int i = 0; i < numerosAleatorios.length; i++) {
+                        int numero = random.nextInt(100);
+                        numerosAleatorios[i] = numero;
+                }
+
+                System.out.print("Numeros Aleatorios: ");
+                for (int numero : numerosAleatorios) {
+                        System.out.print((numero+1) + " ");
+                }
+                System.out.print("\nAntecessor dos Numeros Aleatorios: ");
+                for (int numero : numerosAleatorios) {
+                        System.out.print((numero-1) + "");
+
+                }
+
+                System.out.print("\nSucessores ds Numeros Aleatorios: ");
+                for (int numero : numerosAleatorios) {
+                        System.out.print((numero+1) + " ");
+                }
+
+
         }
-        }
+}
